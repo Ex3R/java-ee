b710af2e72bfc85fcf2ed3f52e540249c45a318e
Terekhov <terekhov_roman@mail.ru>
2020-03-29 02:27:54 +0700

    second part of lab1 was ended

---
+++ lab1/src/main/java/ru/nsu/g16207/terekhov/lab1/model/UserChanges.java
@@ -0,0 +1,15 @@
+package ru.nsu.g16207.terekhov.lab1.model;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Value
+@RequiredArgsConstructor
+public class UserChanges {
+    @NotBlank String user;
+
+    @NotNull Long amountOfChanges;
+}
