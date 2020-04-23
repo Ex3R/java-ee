3b6a778ba79f577a588eb1b122d7a0f6158f8249
Terekhov <terekhov_roman@mail.ru>
2020-04-23 16:39:15 +0700

    compute, fixes

--- lab2/src/main/java/ru/nsu/g16207/terekhov/lab2/model/UidMarks.java
+++ lab2/src/main/java/ru/nsu/g16207/terekhov/lab2/model/UidMarks.java
@@ -3,12 +3,10 @@ package ru.nsu.g16207.terekhov.lab2.model;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
-import javax.validation.constraints.NotBlank;
-
 @Value
 @RequiredArgsConstructor
 public class UidMarks {
-    @NotBlank String uid;
+    int uid;
 
     long amountOfMarks;
 
